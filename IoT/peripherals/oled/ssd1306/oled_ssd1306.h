2c6a9d9dd33a5ca04af32f441bf069a32feac763
alfred <alfred.zhang98@gmail.com>
2023-06-19 21:00:49 +0100

    update code

--- IoT/peripherals/oled/ssd1306/oled_ssd1306.h
+++ IoT/peripherals/oled/ssd1306/oled_ssd1306.h
@@ -4,10 +4,10 @@
 #include <ssd1306_spi_interface.h>
 
 #define SIZE 		    16	
-#define Max_Column	128		
-#define Max_Row		  64		
-#define X_WIDTH 	  128		
-#define Y_WIDTH 	  64
+#define Max_Column	    128		
+#define Max_Row		    64		
+#define X_WIDTH 	    128		
+#define Y_WIDTH 	    64
 
 //function
 void oledSSD1603Setup(void);
